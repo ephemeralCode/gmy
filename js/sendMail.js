6b2b71b936f345164c90c754bdf94696af1bf03c
ephemeralCode <destroid707@yandex.ru>
2023-04-19 00:59:45 +0300

    added sendEmail

---
+++ js/sendMail.js
@@ -0,0 +1,23 @@
+// const contactsBtnSendEmeil = document.querySelector('.Contacts-btnSendEmeil')
+
+// function emeilTest() {
+//     let userEmail = document.querySelector('.Contacts-input').value
+    
+//     Email.send({
+//         SecureToken : "d6ad7707-3972-43dc-90aa-4cab0fb56796",
+
+//         To : '',
+//         From : userEmail,
+//         Subject : "ДЕЛОВОЕ ПРЕДЛОЖЕНИЕ",
+//         Body : `Эта почта хочет связаться с вами ${userEmail}`
+//     })
+//     .then(
+//         (message) => {
+//             message === 'OK' ? alert('Succsess') : message => alert(message)
+//         }
+//     )
+
+//     userEmail.value = ''
+// }
+
+// contactsBtnSendEmeil.addEventListener('click', emeilTest)
